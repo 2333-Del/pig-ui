b6c79fbf6fbd2c2b79383a93426f7165410ebb73
yzy <1450562897@qq.com>
2022-10-08 15:13:11 +0800

    无内容版数据大屏

--- src/views/bigScreen/index.vue
+++ src/views/bigScreen/index.vue
@@ -1,431 +1,64 @@
 <template>
- <div>
-  <header>
-    <div class="headerLeft">
-      <h3>服务数据</h3>
-    </div>
-    <div class="headerCenter">
-      <h1>某某市某某区办税渠道运行状态大数据监控平台</h1>
-    </div>
-    <div class="headerRight">
-      <h3>业务数据</h3>
-    </div>
+  <div>
 
-  </header>
-  <section>
-    <div class="firstCon">
-      <div class="firstCon-left">
-        <h4>各业务平均办理时间/分钟</h4>
-        <div class="firstLeftsec">
-          <div class="contain">
-            <div class="firsttab" id="firsttab1"></div>
-            <p>综合业务</p>
+
+  <div class="header">
+    <h1 class="header-title">大屏数据可视化模板</h1>
+  </div>
+  <div class="wrapper">
+    <div class="content">
+      <div class="col col-l">
+        <div class="xpanel-wrapper xpanel-wrapper-40">
+          <div class="xpanel xpanel-l-t">
+            <div class="title"></div>
           </div>
-          <div class="contain">
-            <div class="firsttab" id="firsttab2"></div>
-            <p>综合业务</p>
-          </div>
-          <div class="contain">
-            <div class="firsttab" id="firsttab3"></div>
-            <p>综合业务</p>
-          </div>
-          <div class="contain">
-            <div class="firsttab" id="firsttab4"></div>
-            <p>综合业务</p>
+        </div>
+        <div class="xpanel-wrapper xpanel-wrapper-60">
+          <div class="xpanel xpanel-l-b">
+            <div class="title"></div>
           </div>
         </div>
       </div>
-      <div class="first-center">
-        <h3>当前大厅等候人数</h3>
-        <div class="firstcenLeft">
-          <div class="flag">
-            <strong>35</strong>
-            <p>综合业务</p>
-          </div>
-          <div class="flag">
-            <strong>35</strong>
-            <p>综合业务</p>
-          </div>
-          <div class="flag">
-            <strong>35</strong>
-            <p>综合业务</p>
+      <div class="col col-c">
+        <div class="xpanel-wrapper xpanel-wrapper-75">
+          <div class="xpanel no-bg">
           </div>
         </div>
-        <div class="firstcenCen">
-          <span>1</span>
-          <span>4</span>
-          <span>9</span>
-        </div>
-        <div class="firstcenRight">
-          <div class="flag">
-            <strong>35</strong>
-            <p>综合业务</p>
-          </div>
-          <div class="flag">
-            <strong>35</strong>
-            <p>综合业务</p>
-          </div>
-          <div class="flag">
-            <strong>35</strong>
-            <p>综合业务</p>
+        <div class="xpanel-wrapper xpanel-wrapper-25">
+          <div class="xpanel xpanel-c-b">
+            <div class="title title-long"></div>
           </div>
         </div>
       </div>
-      <div class="firstCon-right">
-        <h4>各业务平均办理时间/分钟</h4>
-        <div class="firstLeftsec">
-          <div class="contain">
-            <div class="firsttab" id="firsttab5"></div>
-            <p>综合业务</p>
+      <div class="col col-r">
+        <div class="xpanel-wrapper xpanel-wrapper-25">
+          <div class="xpanel xpanel-r-t">
+            <div class="title"></div>
           </div>
-          <div class="contain">
-            <div class="firsttab" id="firsttab6"></div>
-            <p>综合业务</p>
+        </div>
+        <div class="xpanel-wrapper xpanel-wrapper-30">
+          <div class="xpanel xpanel-r-m">
+            <div class="title"></div>
           </div>
-          <div class="contain">
-            <div class="firsttab" id="firsttab7"></div>
-            <p>综合业务</p>
-          </div>
-          <div class="contain">
-            <div class="firsttab" id="firsttab8"></div>
-            <p>综合业务</p>
+        </div>
+        <div class="xpanel-wrapper xpanel-wrapper-45">
+          <div class="xpanel xpanel-r-b">
+            <div class="title"></div>
           </div>
         </div>
       </div>
     </div>
-    <div class="line"></div>
-    <div class="secondCon">
-      <div class="secondLeft">
-        <div class="secondLeftCon">
-          <div class="secondLeftTab">
-            <div class="fig">
-              <p>各办税渠道使用情况</p>
-            </div>
-            <div class="fig">
-              <p>各办税渠道使用情况</p>
-              <em>80人</em>
-            </div>
-            <div class="fig">
-              <p>各办税渠道使用情况</p>
-              <em>120人</em>
-            </div>
-            <div class="fig">
-              <p>各办税渠道使用情况</p>
-              <em>70人</em>
-            </div>
-          </div>
-
-          <div class="secondLeftTab">
-            <div class="fig">
-              <p>接待情况</p>
-            </div>
-            <div class="reception">
-              <div class="recetit">
-                <strong>40人</strong>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <p>本月新办户数</p>
-              </div>
-              <div class="recetit">
-                <strong>40人</strong>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <p>本月新办户数</p>
-              </div>
-              <div class="recetit">
-                <strong>40人</strong>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <span class="changecolor"></span>
-                <p>本月新办户数</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="secondCen">
-        <div class="secondCenCon">
-          <h3>业务办理实时监控</h3>
-          <div class="secondContit">
-            <div class="secondContitleft">
-              <p>今日累计业务办理量</p>
-              <img src="image/line.png" alt="">
-              <ul class="china">
-                <li><a href="">发票代开</a></li>
-                <li><a href="">发票发售</a></li>
-                <li><a href="">纳税申报</a></li>
-                <li><a href="">发票验旧</a></li>
-                <li><a href="">发票认证</a></li>
-              </ul>
-              <img src="image/line.png" alt="">
-              <ul class="number">
-                <li><a href="">20</a></li>
-                <li><a href="">40</a></li>
-                <li><a href="">32</a></li>
-                <li><a href="">34</a></li>
-                <li><a href="">30</a></li>
-              </ul>
-              <img src="image/line.png" alt="">
-            </div>
-            <div class="secondContitright" id="secondContitright">
-              <div class="charbar" id="charbar"></div>
-            </div>
-          </div>
-          <div class="secondContit">
-            <div class="secondContitleft">
-              <p>今日累计业务办理量</p>
-              <img src="image/line.png" alt="">
-              <ul class="china">
-                <li><a href="">发票代开</a></li>
-                <li><a href="">发票发售</a></li>
-                <li><a href="">纳税申报</a></li>
-                <li><a href="">发票验旧</a></li>
-                <li><a href="">发票认证</a></li>
-              </ul>
-              <img src="image/line.png" alt="">
-              <ul class="number">
-                <li><a href="">20</a></li>
-                <li><a href="">40</a></li>
-                <li><a href="">32</a></li>
-                <li><a href="">34</a></li>
-                <li><a href="">30</a></li>
-              </ul>
-              <img src="image/line.png" alt="">
-            </div>
-            <div class="secondContitright" id="secondContitright2">
-              <div class="charbar" id="charbar2"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="secondRight">
-        <div class="secondRightCon">
-          <div class="secondLeftTab">
-            <div class="fig">
-              <p>区域收入情况</p>
-            </div>
-            <div class="secondRighttit">
-              <div class="secondRighttext">
-                <div class="text">
-                  <p>省级以上</p>
-                  <span>442户</span>
-                </div>
-                <div class="text">
-                  <p>省级以上</p>
-                  <span>442户</span>
-                </div>
-                <h4>重点税源户数</h4>
-              </div>
-              <div class="secondRighttext">
-                <div class="secondRighttextChar" id="secondRighttextChar">
-
-                </div>
-                <h4 class="sec">重点税源户数</h4>
-              </div>
-              <div class="secondRighttext">
-                <div class="secondRighttextChar" id="secondRighttextChar1">
-
-                </div>
-                <h4 class="sec">税收占比</h4>
-              </div>
-            </div>
-          </div>
-          <div class="secondLeftTab">
-            <div class="fig">
-              <p>数据统计分析</p>
-            </div>
-            <div class="secondChar" id="secondChar">
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="thirdCon">
-      <div class="thirdLeft">
-        <div class="thirdLeftCon">
-          <div class="secondLeftTab">
-            <div class="fig">
-              <p>当日纳税人咨询最热问题 TOP5</p>
-            </div>
-            <ul class="hottop">
-              <li><span>1</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-              <li><span>2</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-              <li><span>3</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-              <li><span>4</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-              <li><span>5</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="thirdCen">
-        <div class="thirdCenCon">
-          <div class="txt txtleft">
-            <div class="content">
-              <h4>当日各时段大厅人流量曲线图</h4>
-              <div class="contentChar" id="contentChar1"></div>
-            </div>
-          </div>
-          <div class="txt txtright">
-            <div class="content">
-              <h4>当日各时段大厅人流量曲线图</h4>
-              <div class="contentChar" id="contentChar2"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="thirdRight">
-        <div class="thirdRightCon">
-          <div class="secondLeftTab">
-            <div class="fig">
-              <p>当日纳税人咨询最热问题 TOP5</p>
-            </div>
-            <ul class="hottop">
-              <li><span>1</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-              <li><span>2</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-              <li><span>3</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-              <li><span>4</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-              <li><span>5</span><p>如何代开发票？</p><a href="">了解详情</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <footer>
-    <div class="footerleft">
-      <img src="image/right.png" alt="" class="rightimg">
-      <div class="home">
-        <img src="image/home.png" alt="">
-        <button id="homebtn" value="首页">首页</button>
-      </div>
-      <div class="con">
-        <p>当前时间</p>
-        <h4>2018年1月9日 星期四 15:00</h4>
-      </div>
-    </div>
-    <div class="footercen"></div>
-    <div class="footerright"></div>
-  </footer>
- </div>
+  </div>
+  </div>
 </template>
 
 <script>
-
-import '../../../public/static/css/index.css';
-
-// import '../../../public/static/js/jquery.1.10.min.js';
-// import '../../../public/static/js/chars.js';
-
-// import 'http://echarts.baidu.com/gallery/vendors/echarts/echarts-all-3.js';
-
+import "../../../public/static/css/app.css";
 export default {
-  name: "index",
-
-  // initCharts() {
-  //   let self = this
-  //   let myChart = this.echarts.init(this.$refs.echarts)
-  //   console.log(this.$refs.chart) // 绘制图表
-  //   let option = {
-  //     // title: {
-  //     //   text: 'ADP_NP.log',
-  //     // },
-  //     color: ['#66b1ff'],
-  //     tooltip: {},
-  //     animationDurationUpdate: 1500,
-  //     animationEasingUpdate: 'quinticInOut',
-  //     series: [
-  //       {
-  //         type: 'graph',
-  //         layout: 'none',
-  //         symbolSize: 50,
-  //         roam: true,
-  //         label: {
-  //           show: true,
-  //         },
-  //         edgeSymbol: ['circle', 'arrow'],
-  //         edgeSymbolSize: [4, 10],
-  //         edgeLabel: {
-  //           fontSize: 20,
-  //         },
-  //         data: [{
-  //           name: '节点1',
-  //           x: 100,
-  //           y: 300
-  //         }, {
-  //           name: '节点2',
-  //           x: 230,
-  //           y: 200
-  //         }, {
-  //           name: '节点3',
-  //           x: 400,
-  //           y: 250
-  //         }, {
-  //           name: '节点4',
-  //           x: 350,
-  //           y: 350
-  //         }, {
-  //           name: '节点5',
-  //           x: 250,
-  //           y: 400
-  //         }],
-  //         // links: [],
-  //         links: [
-  //           {
-  //             source: '节点1',
-  //             target: '节点2'
-  //           }, {
-  //             source: '节点2',
-  //             target: '节点3'
-  //           }, {
-  //             source: '节点3',
-  //             target: '节点4'
-  //           }, {
-  //             source: '节点4',
-  //             target: '节点5'
-  //           }, {
-  //             source: '节点5',
-  //             target: '节点1'
-  //           }],
-  //         lineStyle: {
-  //           opacity: 0.9,
-  //           width: 2,
-  //           curveness: 0,
-  //         },
-  //       },
-  //     ],
-  //   }
-  //   myChart.setOption(option)
-  //   myChart.on('click', function (params) {
-  //     self.logShow = true
-  //   })
-  // },
-  mounted() {
-    // initCharts()
-  },
-
-
+  name: "index2"
 }
-
 </script>
 
+<style scoped>
+
+</style>
