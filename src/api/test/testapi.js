c5ae53bb44ebed1ab2d40651982fa39f790756a4
yzy <1450562897@qq.com>
2022-10-28 16:33:36 +0800

    vue

---
+++ src/api/test/testapi.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+
+// 查询生成表数据
+export function getdata(query) {
+    var time = new Date().getTime() + '';  //时间戳
+    // time = Hex.encode(DigestUtils.md5(timestamp+ a5b4 +key))
+    var sign = time + 'deviceNoAMT19032315470001' + 'qqzS77nREDO9hQEQCSiyOYbiDL11TVyM';
+    sign = md5(sign)
+
+  return axios({
+    method: 'get',
+    url: 'http://admin.sensor-monitor.cn/v1/getDeviceData',
+    data: {
+      deviceNo: 'AMT19032315470001',
+      timestamp: time,
+      sign: sign,
+    }
+  });
+}
